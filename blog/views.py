79fa66267a785485bb287e9075a35e76ffc669c4
Mateus <mateus-medeiross@hotmail.com>
2018-03-23 23:59:17 -0300

    Last

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
-    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
+    post.visits += 1
+    post.save()
 
 def post_detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	return render(request, 'blog/post_detail.html', {'post': post})
+    post = get_object_or_404(Post, pk=pk)
+    post.visits += 1
+    post.save()
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
@@ -26,7 +27,21 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_new.html', {'form': form})
+
+
+    
+    # try:
+    #     post = Post.objects.get(pk=pk) 
+        
+        
+    # except Post.DoesNotExist:
+    #     return redirect('post_list')
+    # # post = get_object_or_404(Post, pk=pk)
+    # post = Post.objects.filter(visita=+0)
+    # post.visita += 1
+    # post.save()
+    
     
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -41,3 +56,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+    def post_erro404(request):
+        
+        return HttpResponse("Hello, world..")
